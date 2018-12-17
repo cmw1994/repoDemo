43f706af78cafd2e83acd0c138c1ef67d6d55b91
cmw <867079003@qq.com>
2018-12-17 20:39:50 +0800

    asadsa

---
+++ demo1/src/com/cmw/Demo1.java
@@ -0,0 +1,9 @@
+package com.cmw;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        Integer i = 139;
+        Integer j= 139;
+        System.out.println(i==j);
+    }
+}
